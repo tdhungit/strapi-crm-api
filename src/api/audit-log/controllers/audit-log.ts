3522baedc21bd95f1dab6d13b883e2fa885cd7d7
Jacky <tdhungit@gmail.com>
2025-09-09 15:45:56 +0700

    feat: Implement audit log retrieval endpoint

--- src/api/audit-log/controllers/audit-log.ts
+++ src/api/audit-log/controllers/audit-log.ts
@@ -7,27 +7,16 @@ export default factories.createCoreController(
       const user = ctx.state.user;
       const { model, id } = ctx.params;
 
-      const config = await strapi
-        .service('api::metadata.metadata')
-        .getContentTypeConfiguration('plugin::users-permissions.user');
-      const page: number = parseInt(ctx.query.page as string, 10) || 1;
-      const pageSize: number =
-        parseInt(ctx.query.pageSize as string, 10) || config.settings.pageSize;
+      const auditLogs = await strapi.service('api::audit-log.audit-log').find({
+        filters: {
+          model,
+          recordId: id,
+          assigned_user: user.id,
+        },
+        sort: { timestamp: 'desc' },
+        populate: ['assigned_user'],
+      });
 
-      const auditLogs = await strapi.entityService.findMany(
-        'api::audit-log.audit-log',
-        {
-          filters: {
-            model,
-            recordId: id,
-            assigned_user: user.id,
-          },
-          sort: { timestamp: 'desc' },
-          populate: ['assigned_user'],
-          limit: pageSize,
-          start: (page - 1) * pageSize,
-        }
-      );
       return auditLogs;
     },
   })
