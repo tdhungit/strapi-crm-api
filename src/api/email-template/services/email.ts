a77fae97bef689066f3173b281661d68d376598f
Jacky <tdhungit@gmail.com>
2025-10-25 08:33:13 +0700

    refactor(email): Improve email sending logic and add sendMultiple method

--- src/api/email-template/services/email.ts
+++ src/api/email-template/services/email.ts
@@ -27,6 +27,13 @@ export default () => ({
 
     const service = setting.service || 'SMTP';
 
+    if (service === 'SendGrid') {
+      await strapi
+        .service('api::email-template.sendgrid')
+        .send(to, subject, ejsString, data, options);
+      return;
+    }
+
     if (service === 'SMTP') {
       options.from = options.from || setting.from || undefined;
       options.replyTo = options.replyTo || setting.replyTo || undefined;
@@ -50,12 +57,6 @@ export default () => ({
 
       return;
     }
-
-    if (service === 'SendGrid') {
-      await strapi
-        .service('api::email-template.sendgrid')
-        .send(to, subject, ejsString, data, options);
-    }
   },
 
   async sendTemplate(
@@ -72,19 +73,26 @@ export default () => ({
       .getSettings('system', 'mail');
     const setting = settings?.mail || {};
 
-    const template = await strapi.db
-      .query('api::email-template.email-template')
-      .findOne({
-        where: { id: templateId },
-      });
-
-    if (!template) {
-      throw new Error('Template not found');
-    }
-
     const service = setting.service || 'SMTP';
 
+    if (service === 'SendGrid') {
+      await strapi
+        .service('api::email-template.sendgrid')
+        .sendTemplate(to, templateId, data, options);
+      return;
+    }
+
     if (service === 'SMTP') {
+      const template = await strapi.db
+        .query('api::email-template.email-template')
+        .findOne({
+          where: { id: templateId },
+        });
+
+      if (!template) {
+        throw new Error('Template not found');
+      }
+
       options.from = options.from || setting.from || undefined;
       options.replyTo = options.replyTo || setting.replyTo || undefined;
 
@@ -107,11 +115,63 @@ export default () => ({
 
       return;
     }
+  },
+
+  async sendMultiple(
+    templateId: number,
+    data: { to: string; data: any }[],
+    options?: SendMailOptions,
+  ) {
+    options = options || {};
+
+    // Get email settings
+    const settings = await strapi
+      .service('api::setting.setting')
+      .getSettings('system', 'mail');
+    const setting = settings?.mail || {};
+
+    const service = setting.service || 'SMTP';
 
     if (service === 'SendGrid') {
       await strapi
         .service('api::email-template.sendgrid')
-        .sendTemplate(to, templateId, data, options);
+        .sendMultiple(templateId, data, options);
+      return;
+    }
+
+    if (service === 'SMTP') {
+      const template = await strapi.db
+        .query('api::email-template.email-template')
+        .findOne({
+          where: { id: templateId },
+        });
+
+      if (!template) {
+        throw new Error('Template not found');
+      }
+
+      options.from = options.from || setting.from || undefined;
+      options.replyTo = options.replyTo || setting.replyTo || undefined;
+      const emailTemplate = {
+        subject: template.title,
+        html: template.content,
+      };
+
+      for await (const item of data) {
+        await strapi.plugins['email'].services.email.sendTemplatedEmail(
+          {
+            to: item.to,
+            from: options.from || undefined,
+            replyTo: options.replyTo || undefined,
+            cc: options.cc || undefined,
+            bcc: options.bcc || undefined,
+          },
+          emailTemplate,
+          item.data,
+        );
+      }
+
+      return;
     }
   },
 });
