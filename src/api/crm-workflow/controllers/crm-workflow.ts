ba47490c04ed5376a3a0e5fdc63c81eca04712ee
Jacky <tdhungit@gmail.com>
2025-10-27 07:57:24 +0700

    feat(crm-workflow): Implement workflow update and action management

--- src/api/crm-workflow/controllers/crm-workflow.ts
+++ src/api/crm-workflow/controllers/crm-workflow.ts
@@ -35,9 +35,82 @@ export default factories.createCoreController(
                 workflow: entry.id,
                 name: action.name,
                 action_status: 'Ready',
-                metadata: action,
+                metadata: action.metadata || {},
               },
             });
+        }
+      }
+
+      return { data: entry, meta: {} };
+    },
+
+    async update(ctx: Context) {
+      const { id } = ctx.params;
+      const { data } = ctx.request.body;
+
+      const entry = await strapi.db
+        .query('api::crm-workflow.crm-workflow')
+        .update({
+          where: { documentId: id },
+          data: {
+            name: data.name,
+            module: data.module,
+            trigger: data.trigger,
+            workflow_status: data.workflow_status || 'Active',
+            metadata: data.metadata || {},
+          },
+        });
+
+      const existActions = await strapi.db
+        .query('api::crm-workflow-action.crm-workflow-action')
+        .findMany({
+          where: {
+            workflow: {
+              id: entry.id,
+            },
+          },
+        });
+
+      if (data?.actions?.length > 0) {
+        for await (const action of data.actions) {
+          const existAction = existActions.find(
+            (exist) => exist.id === action.id,
+          );
+          if (existAction) {
+            await strapi.db
+              .query('api::crm-workflow-action.crm-workflow-action')
+              .update({
+                where: {
+                  id: existAction.id,
+                },
+                data: {
+                  name: action.name,
+                  action_status: 'Ready',
+                  metadata: action.metadata || {},
+                },
+              });
+          } else {
+            await strapi.db
+              .query('api::crm-workflow-action.crm-workflow-action')
+              .create({
+                data: {
+                  workflow: entry.id,
+                  name: action.name,
+                  action_status: 'Ready',
+                  metadata: action.metadata || {},
+                },
+              });
+          }
+        }
+
+        const actionsToDelete = existActions.filter(
+          (exist) => !data.actions.some((action) => action.id === exist.id),
+        );
+
+        for await (const action of actionsToDelete) {
+          await strapi.db
+            .query('api::crm-workflow-action.crm-workflow-action')
+            .delete({ where: { id: action.id } });
         }
       }
 
