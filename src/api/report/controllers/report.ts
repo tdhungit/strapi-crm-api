09aac80ad0afaa70528692ff99c33c2d6ca20bc2
Jacky <tdhungit@gmail.com>
2025-11-14 19:03:37 +0700

    feat: Implement report generation functionality

--- src/api/report/controllers/report.ts
+++ src/api/report/controllers/report.ts
@@ -1,6 +1,14 @@
 import { factories } from '@strapi/strapi';
+import { Context } from 'koa';
 
 export default factories.createCoreController(
   'api::report.report',
-  ({ strapi }) => ({}),
+  ({ strapi }) => ({
+    async generateReport(ctx: Context) {
+      const { query, filters, module } = ctx.request.body;
+      return await strapi
+        .service('api::report.report')
+        .generateReport(module, filters, query);
+    },
+  }),
 );
--- src/api/report/services/report.ts
+++ src/api/report/services/report.ts
@@ -1,7 +1,21 @@
-/**
- * report service
- */
-
 import { factories } from '@strapi/strapi';
+import { ContentTypeType } from '../../metadata/types';
 
-export default factories.createCoreService('api::report.report');
+export default factories.createCoreService(
+  'api::report.report',
+  ({ strapi }) => ({
+    async generateReport(module: string, filters: any, query: string) {
+      const contentType: ContentTypeType | null = await strapi
+        .service('api::metadata.metadata')
+        .getContentTypeFromCollectionName(module);
+
+      if (!contentType) {
+        throw new Error(`Content type not found for module: ${module}`);
+      }
+
+      return await strapi.entityService.findMany(contentType.uid as any, {
+        filters,
+      });
+    },
+  }),
+);
